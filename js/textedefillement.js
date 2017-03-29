5e1ec51994b29c5c68319059e5a46645d8c8dc89
Galaadseraph <galaad.seraphin@gmail.com>
2017-03-29 15:39:17 +0200

    Update textedefillement.js

--- js/textedefillement.js
+++ js/textedefillement.js
@@ -1,8 +1,9 @@
+/* A mettre dans le HTML baslise script
 var $p = $('p'),
     $cover = $('.cover'),
     width = $p.width(),
     decrement = width / $p.text().length;
-
+*/
 function addChar()
 {    
     $cover.css('width', '-=' + decrement);
