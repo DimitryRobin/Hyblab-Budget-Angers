a24dc75e956127ea3242573f0dd1ab6b06dcdced
Galaadseraph <galaad.seraphin@gmail.com>
2017-03-29 15:18:14 +0200

    Page Footer
    Page basique avec le minimum

---
+++ public/footer.php
@@ -0,0 +1,25 @@
+<?php
+echo '
+<FOOTER class="main-section">
+	<!--Fin du scroll jule et la dette-->
+	<!--jule-->
+	<DIV class="container"><!--Slide entière-->
+		<DIV class="row"><!--image du personnage-->
+			<FIGURE class="text-right wow fadeInUp delay-02s">
+				<IMG src="" alt="jules" />
+			</FIGURE>
+		</DIV>
+	<DIV><!--Bulle-->
+		<P><!--texte a afficher-->Lorem ipsum dolor sit alet, 
+			<SPAN class="cover"</SPAN>
+		</P>
+	</DIV>
+	<!--Ancre vers la carte-->
+	<!--Ancre vers le haut de la page-->
+	
+	</DIV><!--Fin de la slide-->
+	<SCRIPT text="javascript">
+		addChar();
+	</SCRIPT>
+</FOOTER>';
+?>
