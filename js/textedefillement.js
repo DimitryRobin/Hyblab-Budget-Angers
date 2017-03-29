d3c952d2e1f6bd424bed1c7f5c274a4e29d6fb91
Galaadseraph <galaad.seraphin@gmail.com>
2017-03-29 15:20:57 +0200

    javascript : fonction défilement texte
    fonction d'affichage caractère par caractère de texte pour animer les bulles des personnages

---
+++ js/textedefillement.js
@@ -0,0 +1,14 @@
+var $p = $('p'),
+    $cover = $('.cover'),
+    width = $p.width(),
+    decrement = width / $p.text().length;
+
+function addChar()
+{    
+    $cover.css('width', '-=' + decrement);
+    
+    if ( parseInt( $cover.css('width') ) < width )
+    {
+        setTimeout(addChar, 150);
+    }
+}
