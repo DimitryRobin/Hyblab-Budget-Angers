05be12137667b54664cf3bd0144db611626bb6c7
Galaadseraph <galaad.seraphin@gmail.com>
2017-03-30 15:39:51 +0200

    intro plus propre
    presque mais pas encore

--- intro.php
+++ intro.php
@@ -8,35 +8,42 @@
 
 </style>
 
-<?php
-echo '
+
 <section id="introduction">
-          <h2 class="heading">Introduction</h2>
+          <h2 class="heading">Bienvenue dans la famille Maine</h2>
   <div class="content">
     <div class="container clearfix">
       <div class="row">
         <div class="col-md-6">
+			<img class="perso" src="img/Jules2.png" alt="" class="img-responsive img-circle">
 			<div class="bulle">
 				<p id="intro"></p>
 			</div>
         </div>
-        <div class="col-md-2 col-md-offset-1">
-			<img class="perso" src="img/Jules2.png" alt="" class="img-responsive img-circle">
-        </div>
 		<div class="col-md-2 col-md-offset-1">
-			<p id="pere1"></p>
 			<img class="perso" src="img/Laurent2.png" alt="" class="img-responsive img-circle">
+			<div class="bulle">
+				<p id="pere1"></p>
+			</div>
+			
         </div>
-		<div class="col-md-2 col-md-offset-1">
-			<p id="mere1"></p>
+	</div>
+	<div class="row">
+		<div class="col-md-6 col-md-offset-1">
 			<img class="perso" src="img/Chloe2.png" alt="" class="img-responsive img-circle">
+			<div class="bulle">
+			<p id="mere1"></p>
+			</div>
+			
         </div>
 		<div class="col-md-2 col-md-offset-1">
-			<p id="cloe1"></p>
 			<img class="perso" src="img/Sophie2.png" alt="" class="img-responsive img-circle">
+			<div class="bulle">
+			<p id="cloe1"></p>
+			</div>
+			
         </div>
       </div>
     </div>
   </div>
-</section>';
-?>
+</section>
