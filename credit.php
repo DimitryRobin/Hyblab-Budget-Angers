52959c215e062db676aca1489ed7fb8eeb9d15cd
Galaadseraph <galaad.seraphin@gmail.com>
2017-03-30 14:53:33 +0200

    credit image ready
    reste css à mettre

--- credit.php
+++ credit.php
@@ -8,7 +8,7 @@
 					<p>Design EEGP : Antoine BOURLART</p>
 					<p>Développement IMIE : Arnaud Lebeau Remy Delaforterie Dimitri Robin</p>
 					<p>Ville Angers : Julien Rebillard & Antoine Decour</p>
-					<img src="img/Ville_Angers.png" alt="ville d'anger logo"/>
+					<img src="img/DEP49_LOGO.png" alt="ville d'anger logo"/>
 					<p>Code et Licence 
 					
 					<p>Le code du projet est disponible en open-source sur Github.</p>
@@ -16,10 +16,10 @@
 					<p>Deux licences s'appliquent :</p>
 					<p>Licence MIT pour le code source informatique.
 					<a href="https://opensource.org/licenses/MIT"></a>https://opensource.org/licenses/MIT</p>
-					<img src="" alt="logo open source"/>
+					<img src="img/logoopensource.png" alt="logo open source"/>
 					<p>Licence Creative Commons (by) pour tous les contenus créés pendant l’atelier.</p>
 					<a href="https://creativecommons.org/licenses/by/3.0/fr/">https://creativecommons.org/licenses/by/3.0/fr/</a></p>
-					<img src="" alt="logo creative common"/>
+					<img src="img/logocc.png" alt="logo creative common"/>
 				</div>
 			</div>
 		</div>
