3ce3c7f2b25becc24767d3518980efe891cb2c8f
Galaadseraph <galaad.seraphin@gmail.com>
2017-03-30 14:48:45 +0200

    page de credit contenu ready
    a inclure dans l'index et mettre le css et liens images opens source et cc

---
+++ credit.php
@@ -0,0 +1,27 @@
+<section id="credit">
+	<h2 class="heading"></h2>
+	<div class="content">
+		<div class="container clearfix">
+			<div class="row">
+				<div class="col-md-6"> 
+					<p>Edition UCO : Louise Desdomaines Yann Barisset</p>
+					<p>Design EEGP : Antoine BOURLART</p>
+					<p>Développement IMIE : Arnaud Lebeau Remy Delaforterie Dimitri Robin</p>
+					<p>Ville Angers : Julien Rebillard & Antoine Decour</p>
+					<img src="img/Ville_Angers.png" alt="ville d'anger logo"/>
+					<p>Code et Licence 
+					
+					<p>Le code du projet est disponible en open-source sur Github.</p>
+					
+					<p>Deux licences s'appliquent :</p>
+					<p>Licence MIT pour le code source informatique.
+					<a href="https://opensource.org/licenses/MIT"></a>https://opensource.org/licenses/MIT</p>
+					<img src="" alt="logo open source"/>
+					<p>Licence Creative Commons (by) pour tous les contenus créés pendant l’atelier.</p>
+					<a href="https://creativecommons.org/licenses/by/3.0/fr/">https://creativecommons.org/licenses/by/3.0/fr/</a></p>
+					<img src="" alt="logo creative common"/>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
