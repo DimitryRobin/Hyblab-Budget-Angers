4661242b2f4d9e5208f5df30384c3fc76e1b2735
Galaadseraph <galaad.seraphin@gmail.com>
2017-03-30 12:14:10 +0200

    texte dette upload
    plus qu'à mettre en forme avec les images

--- Dettes.php
+++ Dettes.php
@@ -5,13 +5,25 @@ echo '
   <div class="content">
     <div class="container clearfix">
       <div class="row">
-        <div class="col-md-6"> 
-          <p class="lead">This can be an about section or anything else ;)</p>
-          <p>Impossible considered invitation him men instrument saw celebrated unpleasant. Put rest and must set kind next many near nay. He exquisite continued explained middleton am. Voice hours young woody has she think equal. Estate moment he at on wonder at season little. Six garden result summer set family esteem nay estate. End admiration mrs unreserved discovered comparison especially invitation. </p>
-          <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery. </p>
+        <div class="col-md-6">
+			<div ><!--/*bulle de jule 1*/-->
+				<p id="juleinvest"></p>
+			</div>
+			<div><!--/*Texte d\'information*/-->  
+			<p>Pour financer ses investissements, la ville emprunte, utilise son épargne et reçoit des
+					subventions de l\'Europe, l\'État, la région, le département puis l\'agglomération.</p>
+			</div>	
+			<div ><!--/*bulle de jule 2*/-->
+				<p id="juledette"></p>
+			</div>
+			<div ><!--/*bulle du père*/-->
+				<p id="peredette"></p>	
+			</div>
         </div>
         <div class="col-md-5 col-md-offset-1">
-          <p><img src="screen/img/about.jpg" alt="" class="img-responsive img-circle"></p>
+			<figure>
+				<img src="screen/img/about.jpg" alt="" class="img-responsive img-circle">
+			</figure>
         </div>
       </div>
     </div>
