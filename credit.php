e9f74484efdac40ce2479ba41ee598d92019e4af
Galaadseraph <galaad.seraphin@gmail.com>
2017-03-30 15:04:54 +0200

    credit propre
    ou presque

--- credit.php
+++ credit.php
@@ -1,3 +1,11 @@
+<style>
+#credit img {
+    max-width: 250px;
+    max-height: 200px;
+      margin: 0 auto;
+      padding: 0 30px;
+}
+</style>
 <section id="credit">
 	<h2 class="heading"></h2>
 	<div class="content">
@@ -5,19 +13,19 @@
 			<div class="row">
 				<div class="col-md-6"> 
 					<p>Edition UCO : Louise Desdomaines Yann Barisset</p>
-					<p>Design EEGP : Antoine BOURLART</p>
-					<p>Développement IMIE : Arnaud Lebeau Remy Delaforterie Dimitri Robin</p>
+					<p>Design EEGP : Antonin BOURLART</p>
+					<p>DÃ©veloppement IMIE : Arnaud Lebeau Remy Delaforterie Dimitri Robin</p>
 					<p>Ville Angers : Julien Rebillard & Antoine Decour</p>
-					<img src="img/DEP49_LOGO.png" alt="ville d'anger logo"/>
+					<img src="img/DEP49_LOGO.jpeg" alt="ville d'anger logo"/>
 					<p>Code et Licence 
-					
+				</div>
+				<div class="col-md-6"> 		
 					<p>Le code du projet est disponible en open-source sur Github.</p>
-					
 					<p>Deux licences s'appliquent :</p>
 					<p>Licence MIT pour le code source informatique.
 					<a href="https://opensource.org/licenses/MIT"></a>https://opensource.org/licenses/MIT</p>
 					<img src="img/logoopensource.png" alt="logo open source"/>
-					<p>Licence Creative Commons (by) pour tous les contenus créés pendant l’atelier.</p>
+					<p>Licence Creative Commons (by) pour tous les contenus crÃ©Ã©s pendant l'atelier.</p>
 					<a href="https://creativecommons.org/licenses/by/3.0/fr/">https://creativecommons.org/licenses/by/3.0/fr/</a></p>
 					<img src="img/logocc.png" alt="logo creative common"/>
 				</div>
