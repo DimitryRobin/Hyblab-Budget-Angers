11e8ebed984a15ec32f9ec10d6df2fcfa3b1b0b6
Galaadseraph <galaad.seraphin@gmail.com>
2017-03-30 15:06:08 +0200

    credit lien debugé

--- credit.php
+++ credit.php
@@ -23,7 +23,7 @@
 					<p>Le code du projet est disponible en open-source sur Github.</p>
 					<p>Deux licences s'appliquent :</p>
 					<p>Licence MIT pour le code source informatique.
-					<a href="https://opensource.org/licenses/MIT"></a>https://opensource.org/licenses/MIT</p>
+					<a href="https://opensource.org/licenses/MIT">https://opensource.org/licenses/MIT</a></p>
 					<img src="img/logoopensource.png" alt="logo open source"/>
 					<p>Licence Creative Commons (by) pour tous les contenus créés pendant l'atelier.</p>
 					<a href="https://creativecommons.org/licenses/by/3.0/fr/">https://creativecommons.org/licenses/by/3.0/fr/</a></p>
